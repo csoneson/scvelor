d3f5b9fcecba4d2ba3b6e954cb254a68db38b715
Charlotte Soneson <charlottesoneson@gmail.com>
2020-06-06 14:21:28 +0200

    Initial commit

---
+++ R/basilisk.R
@@ -0,0 +1,11 @@
+#' @importFrom basilisk BasiliskEnvironment
+scvelo <- basilisk::BasiliskEnvironment("scvelo",
+                                        pkgname="BiocVelo",
+                                        packages=c("numpy==1.18.4",
+                                                   "scipy==1.4.1",
+                                                   "numba==0.49.1",
+                                                   "matplotlib==3.2.1",
+                                                   "scikit-learn==0.23.0",
+                                                   "h5py==2.10.0",
+                                                   "click==7.1.2"),
+                                        pip="scvelo==0.2.1")
---
+++ README.md
@@ -0,0 +1,5 @@
+## scvelor
+
+[![R build status](https://github.com/csoneson/scvelor/workflows/R-CMD-check/badge.svg)](https://github.com/csoneson/scvelor/actions)
+
+R wrapper for the scVelo python package for RNA velocity analysis.
